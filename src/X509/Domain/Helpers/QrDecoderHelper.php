f85d01dfea0bb31db84cd411fcff226418487464
Vitaliy <theyamshikov@yandex.ru>
2020-11-24 18:11:28 +0600

    upd

--- src/X509/Domain/Helpers/QrDecoderHelper.php
+++ src/X509/Domain/Helpers/QrDecoderHelper.php
@@ -3,39 +3,69 @@
 namespace ZnCrypt\Pki\X509\Domain\Helpers;
 
 use Illuminate\Support\Collection;
+use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
 use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
 
 class QrDecoderHelper
 {
 
-    public static function extractData(array $qrs, string $nameSpace = '')
+    public static function extractXml(array $qrs)
     {
         $collection = self::extract($qrs);
         $data = self::collectionToBin($collection);
         $xmlContent = self::unZip($data);
+        return $xmlContent;
+    }
+
+    public static function extractData(array $qrs)
+    {
+        $xmlContent = self::extractXml($qrs);
 //        dd($xmlContent);
         $xml = new \Symfony\Component\Serializer\Encoder\XmlEncoder();
-        return $xml->decode($xmlContent, 'xml');
+        $context = [
+            'xml_format_output' => true,
+            'xml_root_node_name' => 'data',
+//    'xml_encoding' => 'UTF-8',
+//    'remove_empty_tags' => true,
+        ];
+        return $xml->decode($xmlContent, 'xml', $context);
 //        dd($ssss);
 //        $xmlArray = \ZnCrypt\Pki\X509\Domain\Helpers\XmlHelper::parseXml($xmlContent, $nameSpace);
 //        $xmlArray = $xmlArray['data'];
 //        return $xmlArray;
     }
 
-    private static function extract(array $qrs): Collection
+    public static function extract(array $qrs): Collection
     {
+        $xml = new \Symfony\Component\Serializer\Encoder\XmlEncoder;
+        $context = [
+            'xml_format_output' => true,
+            'xml_root_node_name' => 'data',
+//    'xml_encoding' => 'UTF-8',
+//    'remove_empty_tags' => true,
+        ];
+
         $collection = new Collection();
+        $arer = [];
         foreach ($qrs as $xmlContent) {
-            $item = XmlHelper::parseXml($xmlContent);
-            $collection->add($item['BarcodeElement']);
+//            $item = XmlHelper::parseXml($xmlContent);
+            $item = $xml->decode($xmlContent, 'xml');
+//            dd($item);
+            $collection->add($item);
+            $arr[] = $item;
         }
+        ArrayHelper::multisort($arr, 'elementNumber');
+        return new Collection($arr);
+        /*dd($arr);
         $collection = $collection->sortBy(function ($value, $key) {
-            return $value['elementNumber'];
+//            dd($value['elementNumber']);
+            return intval($value['elementNumber']);
         });
-        return $collection;
+        dd($collection);
+        return $collection;*/
     }
 
-    private static function collectionToBin(Collection $collection): string
+    public static function collectionToBin(Collection $collection): string
     {
         $data = '';
         foreach ($collection as $item) {
@@ -45,29 +75,26 @@ class QrDecoderHelper
         return $data;
     }
 
-    private static function unZip($data)
+    public static function unZip($data)
     {
-<<<<<<< HEAD:src/X509/Domain/Helpers/QrDecoderHelper.php
         $zipFile = tempnam(sys_get_temp_dir(), 'qrZip');
-=======
-        FileHelper::createDirectory($xmlDir);
-        $zipFile = $xmlDir . '/zip.zip';
->>>>>>> 8128b985a9e767c74090b129d6bd41a3364c3f88:src/X509/Domain/Helpers/QrHelper.php
+        //dd($zipFile);
+        //FileHelper::createDirectory($xmlDir);
+        //$zipFile = $xmlDir . '/zip.zip';
         FileHelper::save($zipFile, $data);
         $zip = new \ZipArchive();
         $res = $zip->open($zipFile);
+//        dd($res);
         if ($res === TRUE) {
             $xmlContent = $zip->getFromName('one');
             $zip->close();
         } else {
             throw new Exception('Zip not opened!');
         }
-<<<<<<< HEAD:src/X509/Domain/Helpers/QrDecoderHelper.php
-=======
-        $xmlContent = FileHelper::load($xmlDir . '/one');
+//        $xmlContent = FileHelper::load($xmlDir . '/one');
+//        dd($xmlContent);
         unlink($zipFile);
-        unlink($xmlDir . '/one');
->>>>>>> 8128b985a9e767c74090b129d6bd41a3364c3f88:src/X509/Domain/Helpers/QrHelper.php
+//        unlink($xmlDir . '/one');
         return $xmlContent;
     }
 }
