6008b9b131e85eda7797a705feb052834745d1e8
Vitaliy <theyamshikov@yandex.ru>
2021-06-30 23:20:14 +0600

    upd

--- src/XmlDSig/Domain/Libs/KeyLoaders/DirectoryKeyLoader.php
+++ src/XmlDSig/Domain/Libs/KeyLoaders/DirectoryKeyLoader.php
@@ -2,6 +2,7 @@
 
 namespace ZnCrypt\Pki\XmlDSig\Domain\Libs\KeyLoaders;
 
+use Illuminate\Support\Collection;
 use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
 use ZnCore\Base\Libs\DotEnv\DotEnv;
 use ZnCore\Domain\Helpers\EntityHelper;
@@ -21,12 +22,27 @@ class DirectoryKeyLoader
         'p12' => 'rsa.p12',
     ];
 
+    public function all(): Collection
+    {
+        $pkiDirectory = DotEnv::get('PKI_DIRECTORY');
+        $files = FileHelper::scanDir($pkiDirectory);
+        $collection = new Collection();
+        foreach ($files as $file) {
+            $fileNmae = $pkiDirectory . '/' . $file;
+            if(is_dir($fileNmae)) {
+                $keyEntity = $this->load($file);
+                $collection->add($keyEntity);
+            }
+        }
+        return $collection;
+    }
+    
     public function remove(string $name): void
     {
         $pkiDirectory = DotEnv::get('PKI_DIRECTORY');
         $directory = $pkiDirectory . '/' . $name;
         FileHelper::removeDirectory($directory);
-        //FileHelper::createDirectory('user');
+        FileHelper::createDirectory($directory);
     }
 
     public function load(string $name): KeyEntity
@@ -41,7 +57,7 @@ class DirectoryKeyLoader
                 $data[$attributeName] = FileHelper::load($file);
             }
         }
-        
+
         $userKeyEntity = new KeyEntity;
         EntityHelper::setAttributes($userKeyEntity, $data);
         $userKeyEntity->setName($name);
@@ -53,65 +69,11 @@ class DirectoryKeyLoader
         $pkiDirectory = DotEnv::get('PKI_DIRECTORY');
         $directory = $pkiDirectory . '/' . $name;
         $data = EntityHelper::toArray($keyEntity);
+        unset($data['name']);
         foreach ($data as $attributeName => $value) {
             if(!empty($value)) {
                 $fileName = $this->names[$attributeName];
                 $file = $directory . '/' . $fileName;
-                FileHelper::save($file, $value);
-            }
-        }
-    }
-
-    
-    
-    
-    
-    
-    public function load111(string $directory): KeyEntity
-    {
-        $directory = $directory ?? DotEnv::get('PKI_DIRECTORY');
-        $names = [
-            'certificate' => 'certificate.pem',
-            'certificateRequest' => 'certificateRequest.pem',
-            'privateKeyPassword' => 'password.txt',
-            'privateKey' => 'private.pem',
-            'publicKey' => 'public.pem',
-            'p12' => 'rsa.p12',
-        ];
-
-        $data = [];
-        foreach ($names as $attributeName => $fileName) {
-            $file = $directory . '/' . $fileName;
-            if(file_exists($file)) {
-                $data[$attributeName] = FileHelper::load($file);
-            }
-        }
-
-        $userKeyEntity = new KeyEntity;
-        EntityHelper::setAttributes($userKeyEntity, $data);
-        return $userKeyEntity;
-    }
-
-    public function save1111(string $directory, KeyEntity $keyEntity): void
-    {
-        $directory = $directory ?? DotEnv::get('PKI_DIRECTORY');
-        $names = [
-            'certificate' => 'certificate.pem',
-            'certificateRequest' => 'certificateRequest.pem',
-            'csr' => 'certificateRequest.pem',
-            'privateKeyPassword' => 'password.txt',
-            'privateKey' => 'private.pem',
-            'publicKey' => 'public.pem',
-            'p12' => 'rsa.p12',
-        ];
-
-        $data = EntityHelper::toArray($keyEntity);
-
-        foreach ($data as $attributeName => $value) {
-            if(!empty($value)) {
-                $fileName = $names[$attributeName];
-                $file = $directory . '/' . $fileName;
-                //dd($file);
                 FileHelper::save($file, $value);
             }
         }
